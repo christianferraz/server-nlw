370069aa081b38658ae0207e46839cceed815d40
Christian Ferraz <christianferraz@gmail.com>
2022-09-13 23:13:48 -0400

    First commit

---
+++ build/server.js
@@ -0,0 +1,8 @@
+import express from "express";
+const app = express();
+app.get('/', (req, res) => {
+    return res.status(200).json([
+        { id: 1, name: 'John', age: 34 }
+    ]);
+});
+app.listen(8000);
